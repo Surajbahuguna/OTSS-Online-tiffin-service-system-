89bc3f41aa7a5cc87afb0ec6f0eb038692bb4ace
Suraj Bahuguna <88368067+Surajbahuguna@users.noreply.github.com>
2023-06-25 11:48:28 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+	<div class="footer">
+		<div class="container">
+			<p class="wow fadeInLeft" data-wow-delay="0.1s">&copy; Online Tiffin Service System In Rishikesh &nbsp;</p>
+			
+		</div>
+	</div>
